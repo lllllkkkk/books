aaaab165c6cc396b86a4efb4fb59e0c2b37de83e
GuangTong <67990506+lllllkkkk@users.noreply.github.com>
2021-09-28 15:36:29 +0800

    Delete src directory

--- src/config/index.js
+++
@@ -1,8 +0,0 @@
-export default {
-    baseURL: {
-        development: "http://192.168.1.97:8092/managementSystem/",
-        test: 'http://ip1:port1',
-        production: 'http://ip2:port2'
-    }
-  }
-  
